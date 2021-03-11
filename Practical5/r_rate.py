64a9428fc600db7fd12856dfd2448e3aa11db856
3200110784 <79952858+3200110784@users.noreply.github.com>
2021-03-11 16:27:53 +0800

    Update r_rate.py

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,12 +1,12 @@
 print("please enter r rate here")
 r=float(input("r_rate="))
-n=84
+n=84 #the number of students
 #initialize
-for i in range(1,6):
+for i in range(1,6):#count
  i=i+1
  n=n*r
 #calculate the number of infected
 m=str(int(n))
 #to obtain an integer
-print("the number of infected individuals after 5 rounds of infection for a given r rate "+ str(r) +" is "+m)
+print("the number of infected individuals after 5 rounds of infection for a given r rate "+ str(r) +" is "+m)#get the answer
 exit()
