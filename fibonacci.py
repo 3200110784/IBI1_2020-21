6ecd4db292ba670effe698ee52dcf221fdd33f68
3200110784 <79952858+3200110784@users.noreply.github.com>
2021-03-11 16:22:32 +0800

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+a=1
+b=1
+c=0
+print(a)
+print(b)
+#first and second values
+number=3
+#count number
+for number in range(3,14):
+ c=a+b
+ a=b
+ b=c
+ number=number+1
+ print(c)
+ #output
+exit()
+
