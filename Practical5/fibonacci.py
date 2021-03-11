8228d9f391b6d14b3f761439eb6fa8f2f8328e0a
3200110784 <79952858+3200110784@users.noreply.github.com>
2021-03-11 16:30:13 +0800

    Update fibonacci.py

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,10 +1,10 @@
-a=1
-b=1
-c=0
+a=1  #the first number
+b=1  #the secound number
+c=0  #next number
 print(a)
 print(b)
 #first and second values
-number=3
+number=3 #this is not nessary
 #count number
 for number in range(3,14):
  c=a+b
